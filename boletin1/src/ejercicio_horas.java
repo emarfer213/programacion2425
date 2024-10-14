80259a6a640f28e0887fab703efe45651424967c
pc-emarfer213 <emiliomartinf2002@gmail.com>
2024-10-14 11:13:17 +0200

    comienzo

--- boletin1/src/ejercicio_horas.java
+++ boletin1/src/ejercicio_horas.java
@@ -4,7 +4,10 @@ public class ejercicio_horas {
 
     public static void main(String[] args) {
 
+        int dias;
         Scanner sc = new Scanner(System.in);
+
+        int hf = 0,mf = 0,sf = 0;
 
         System.out.println("dime la hora");
 
@@ -29,6 +32,24 @@ public class ejercicio_horas {
         System.out.println("cuantos segundos quieres sumar");
 
         int n6 = sc.nextInt();
+
+        sf = (n3 + n6) % 60;
+
+        mf += (n3 + n6) / 60;
+
+        hf += (n1 + n4) / 60;
+
+        mf = (n2 + n5) % 60;
+
+        dias = (n1 + n4) / 24;
+
+        hf = (hf + n4) % 24;
+
+        System.out.printf("la hora resultante es %02d:%02d:%02d" ,hf,mf,sf);
+        System.out.println(" han pasado " +dias+ " dias");
+
+
+
 
 
 
