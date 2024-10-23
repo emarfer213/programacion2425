c01440f4267d8aa0ea9f82a5e621b42b5154461a
pc-emarfer213 <emiliomartinf2002@gmail.com>
2024-10-23 10:27:12 +0200

    arrays

---
+++ boletin1/src/b/tema3/ejercicio3.java
@@ -0,0 +1,18 @@
+package b.tema3;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ejercicio3 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("dame la cantidad de numeros que quieres usar");
+
+        int repe = sc.nextInt();
+
+
+
+    }
+}
---
+++ boletin1/src/b/tema3/ejercicio4.java
@@ -0,0 +1,58 @@
+package b.tema3;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class ejercicio4 {
+
+    static final int size=6000;
+    static final int seed=21;
+    public static void main(String[] args) {
+
+        int[] aleatorio = new int[size];
+        rellenaArray(aleatorio);
+        digitofinal(aleatorio);
+
+        System.out.println(mediaArray(aleatorio));
+
+    }
+
+    private static void rellenaArray(int[] aleatorio){
+
+        for (int i = 0; i < aleatorio.length; i++) {
+            Random random = new Random(seed);
+            int random1 = random.nextInt(101);
+            aleatorio[i] = random1;
+        }
+
+    }
+
+    private static double mediaArray(int[] aleatorio) {
+        double media = 0;
+        for (int i = 0; i < aleatorio.length; i++) {
+            media += aleatorio[i];
+        }
+        return media/ aleatorio.length;
+    }
+
+    private static void digitofinal(int[] aleatorio){
+        int [] repeticiones = new int[10];
+
+        for (int i = 0; i < aleatorio.length; i++) {
+            int terminaciones = aleatorio[i] % 10;
+            repeticiones[terminaciones]++;
+
+        }
+        int max = -1;
+        int indicemax = -1;
+        for (int i = 0; i < repeticiones.length; i++) {
+            if (repeticiones[i] > max);
+            max = repeticiones[i];
+            indicemax = i;
+        }
+
+        System.out.println(Arrays.toString(repeticiones));
+    }
+
+
+}
