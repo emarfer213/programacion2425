c902dff405f356c1115a0fd1e36cf0ac9ee7e23a
emarfer213 <emarfer213@g.educaan.es>
2024-10-28 21:40:40 +0100

    arrays

--- boletin1/src/b/tema3/ejercicio11.java
+++ boletin1/src/b/tema3/ejercicio11.java
@@ -13,31 +13,16 @@ public class ejercicio11 {
     }
 
     private static int[] creaArray() {
-        System.out.println("cuantos numeros quieres meter para el primer array");
-        int n1 = sc.nextInt();
 
-        int[] array1 = new int[n1];
+        int[] array1 = {1,2,3,4,5};
+        int[] array2 = {1,7,8,9,6};
 
+        int[] arrayFinal = new int[0];
         for (int i = 0; i < array1.length; i++) {
-            System.out.println("dime un numero");
-            array1[i] = sc.nextInt();
-        }
+            if (array2[i] == array1[i]);
+            assert false;
+            arrayFinal[i] += array2[i];
 
-
-        System.out.println("mete la misma cantidad de numeros para el segundo array");
-
-
-        int[] array2 = new int[n1];
-
-        for (int i = 0; i < array2.length; i++) {
-            System.out.println("dime un numero");
-            array2[i] = sc.nextInt();
-        }
-
-        int[] arrayFinal = null;
-        for (int i = 0; i < array1.length; i++) {
-            if (array2[i] == array1[i]) ;
-            arrayFinal = new int[]{array2[i]};
         }
 
         return arrayFinal;
