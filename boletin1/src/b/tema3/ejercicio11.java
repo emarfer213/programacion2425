d853efaf45b03cfaead4ce6af67fc995ecf19b91
emarfer213 <emarfer213@g.educaan.es>
2024-10-28 20:45:54 +0100

    arrays

---
+++ boletin1/src/b/tema3/ejercicio11.java
@@ -0,0 +1,25 @@
+package b.tema3;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ejercicio11 {
+
+    /*public static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int[] primerArray = {7, 2, 4, 5, 1};
+        int[] segundoArray = {7, 3, 4, 8, 1};
+        int[] arrayFinal = creaArray();
+    }
+
+
+    private static int[] creaArray(int[] arrayFinal) {
+
+        for (int i = 0; i < arrayFinal.length; i++) {
+
+           int array = 0;
+
+        }
+    }*/
+}
