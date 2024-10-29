deaa5b0dd5a69de0ca13fffd560ac8456d46c2dc
pc-emarfer213 <emiliomartinf2002@gmail.com>
2024-10-29 14:03:26 +0100

    arrays

---
+++ boletin1/src/b/tema3/ejercicio11.java
@@ -0,0 +1,37 @@
+package b.tema3;
+
+import java.util.Arrays;
+
+public class ejercicio11 {
+
+    public static void main(String[] args) {
+
+        int[] array1 = {1,2,3,7,2};
+        int[] array2 = {7,3,2,9,1};
+        int[] arrayFinal = elementoscomunes(array1,array2);
+        System.out.println(Arrays.toString(arrayFinal));
+    }
+
+    private static int[] elementoscomunes(int[] array1, int [] array2){
+        int indicedupli=0;
+        int[] nuevoArray = new int[array1.length];
+
+        for (int i = 0; i < array1.length; i++) {
+            if (buscarnumero(array2, array1[i]) && !buscarnumero(nuevoArray, array1[i])) {
+                nuevoArray[indicedupli] = array1[i];
+                indicedupli++;
+            }
+        }
+        return nuevoArray;
+    }
+
+    private static boolean buscarnumero(int[] array, int num) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == num) {
+                return true;
+            }
+        }
+        return false;
+
+    }
+}
