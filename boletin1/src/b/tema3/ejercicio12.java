c6ddc8221682a13967cafcc7e2639622db9d9be8
pc-emarfer213 <emiliomartinf2002@gmail.com>
2024-10-29 14:42:49 +0100

    arrays

--- boletin1/src/b/tema3/ejercicio12.java
+++ boletin1/src/b/tema3/ejercicio12.java
@@ -4,20 +4,21 @@ import java.util.Arrays;
 
 public class ejercicio12 {
 
-    /*public static void main(String[] args) {
+   /* public static void main(String[] args) {
 
         int[] array1 = {1, 4, 3, 7, 2};
         int[] arrayFinal = elementosOrdenados(array1);
         System.out.println(Arrays.toString(arrayFinal));
     }
-    private static int[] elementosOrdenados(int[] array1){
+    private static void elementosOrdenados(int[] array1){
         for (int i = 0; i < array1.length; i++) {
             for (int j = 0; j < array1.length-1; j++) {
+                if (array1[i] > array1[i+1]) {
 
+                }
             }
         }
 
-        return arrayFinal;
     }*/
 
 }
