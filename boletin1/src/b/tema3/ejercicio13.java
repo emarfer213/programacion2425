9928e0f6a522f4af85df12e9033f04ec90a9a89b
pc-emarfer213 <emiliomartinf2002@gmail.com>
2024-10-30 10:26:18 +0100

    arrays

---
+++ boletin1/src/b/tema3/ejercicio13.java
@@ -0,0 +1,35 @@
+package b.tema3;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ejercicio13 {
+    /*static Scanner sc = new Scanner(System.in);
+    static int indice = 0;
+
+    public static void main(String[] args) {
+        int[] pila = new int[5];
+        int opcion;
+
+        do {
+            System.out.println("elije una opcion seleccionando el numero: 1.apilar, 2.desapilar o 3.mostrar la pila");
+            opcion = sc.nextInt();
+
+            if (opcion==1);
+                apilar(pila);
+
+            if (opcion == 2);
+                desapilar(pila);
+
+        } while (opcion != 3);
+        System.out.println(Arrays.toString(pila));
+    }
+
+    private static int[] apilar(int[] pila){
+
+    }
+
+    private static int[] desapilar(int[] pila){
+
+    }*/
+}
---
+++ boletin1/src/b/tema3/ejercicio14.java
@@ -0,0 +1,19 @@
+package b.tema3;
+
+import comun.MiEntradaSalida;
+
+public class ejercicio14 {
+    private static int[] cola;
+    private static int[] numElemento;
+
+    public static void main(String[] args){
+        int numero = MiEntradaSalida.solicitarCaracter("introduce un numero");
+
+    }
+    public static void encolar(int n){
+
+    }
+    public static void desencolar(){
+
+    }
+}
