8dc2c48d7bfb0f11e2d355c9e1f4063ea9886d94
emarfer213 <emarfer213@g.educaan.es>
2024-11-13 19:30:07 +0100

    buscaminas

--- boletin1/src/buscaminas.java
+++ boletin1/src/buscaminas.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+import java.util.Scanner;
 
 import static java.lang.Math.random;
 
@@ -8,10 +9,24 @@ public class buscaminas {
     private static final int TAM_MATRIZ = 10;
     private static final int MINAS_A_PLANTAR = 10;
     private static final int MINA=-1;
+
     public static void main(String[] args) {
         generarMatrices(TAM_MATRIZ);
         plantarMinas(MINAS_A_PLANTAR);
-        visualizaTablero();
+        tableroJugador();
+
+    }
+
+    public static void mostrarTablero(String[][] tableroJugador, int[][] matrizLevantadas){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("escribe cordenadas");
+        System.out.println("cordenada x");
+        int i = Integer.parseInt(sc.nextLine())-1;
+        System.out.println("cordenada y");
+        int j = Integer.parseInt(sc.nextLine())-1;
+
+
+
 
     }
 
@@ -20,12 +35,22 @@ public class buscaminas {
         matrizLevantadas = new boolean[tam][tam];
     }
 
+
     public static void plantarMinas(int numMinas){
         for (int i = 0; i < numMinas; i++) {
             int cordenadaX = (int) (Math.random()*TAM_MATRIZ);
             int cordenadaY = (int) (Math.random()*TAM_MATRIZ);
             if (matrizMinas[cordenadaX][cordenadaY]!=MINA){
                 matrizMinas[cordenadaX][cordenadaY]=MINA;
+                for (int j = cordenadaX-1; j <= cordenadaX+1; j++) {
+                    for (int k = cordenadaY-1; k <= cordenadaY+1; k++) {
+                        if (j >= 0 && j < matrizMinas.length && k >= 0 && k < matrizMinas.length) {
+                            if (matrizMinas[j][k] > -1) {
+                                matrizMinas[j][k]++;
+                            }
+                        }
+                    }
+                }
             }
             else {
                 i--;
@@ -34,7 +59,7 @@ public class buscaminas {
         }
     }
 
-    public static void visualizaTablero(){
+    public static void tableroJugador(){
         for (int i = 0; i < TAM_MATRIZ; i++) {
             for (int j = 0; j < TAM_MATRIZ; j++) {
                 if (matrizLevantadas[i][j]){
@@ -47,5 +72,20 @@ public class buscaminas {
             System.out.println();
         }
     }
+
+    /*public static void visualizaTablero(){
+        for (int i = 0; i < TAM_MATRIZ; i++) {
+            for (int j = 0; j < TAM_MATRIZ; j++) {
+                if (matrizLevantadas[i][j]){
+                    System.out.printf("%2d | ", matrizMinas[i][j]);
+
+                } else {
+                    System.out.printf("%2d | ",matrizMinas[i][j]);
+                }
+            }
+            System.out.println();
+        }
+    }*/
+
 
 }
