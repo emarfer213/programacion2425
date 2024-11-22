2bf4a8c1e66eb54b0dba5e22c6e9e9a982cea8ed
pc-emarfer213 <emiliomartinf2002@gmail.com>
2024-11-22 10:22:50 +0100

    tema4

--- boletin1/src/tema4/Cuenta.java
+++ boletin1/src/tema4/Cuenta.java
@@ -5,32 +5,57 @@ public class Cuenta {
     private String numeroCuenta;
     private String titular;
     private double saldo;
-
+    private int contadorRetirada;
+    private int contadoringreso;
 
 
     public Cuenta(String numeroCuenta, String titular, double saldoInicial) throws CuentaException{
         this.numeroCuenta = numeroCuenta;
         this.titular = titular;
+        contadorRetirada=0;
+        contadoringreso=0;
+
         this.setSaldo(saldoInicial);
     }
 
-    private void setSaldo(double saldo) throws CuentaException {
+    public double getSaldo(double saldo){
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) throws CuentaException {
         if (saldo < 0){
             throw new CuentaException("el saldo no puede ser negativo");
         }
         this.saldo=saldo;
     }
 
-    private double dineroRetirar(double saldo){
+
+
+    public void sacarDinero(double retirada){
 
         System.out.println("indicame cuanto dinero quieres retirar");
         int cantidadRetirar = sc.nextInt();
 
         if (cantidadRetirar > saldo){
             System.out.println("no puedes sacar mas dinero del que ya tienes");
+        } else if (cantidadRetirar <= 0) {
+            System.out.println("no puedes retirar una cantidad negativa o cero");
         }
-
-        return cantidadRetirar;
+        this.saldo -= retirada;
+        contadorRetirada--;
     }
+
+    public void meterDinero(double ingreso) throws CuentaException {
+        System.out.println("indicame cuanto dinero quieres meter");
+        int cantidadMeter = sc.nextInt();
+
+        if (ingreso<=0){
+            throw new CuentaException("el ingreso no puede ser cero o menor");
+        }
+        this.saldo += ingreso;
+        contadoringreso++;
+    }
+
+
 
 }
--- boletin1/src/tema4/principalCuenta.java
+++ boletin1/src/tema4/principalCuenta.java
@@ -1,11 +1,52 @@
 package tema4;
+import java.util.Scanner;
 
 public class principalCuenta {
-    public static void main(String[] args){
+    public static void main(String[] args) throws CuentaException {
+
+        int dineroSacar;
+        int dineroMeter;
+        int opcion;
+        Scanner entrada= new Scanner(System.in);
+
+        Cuenta c1;
         try {
-            Cuenta c1 = new Cuenta("1234", "Emilio",10);
-        } catch (CuentaException e) {
-            System.out.println(e.getMessage());
+             c1 = new Cuenta("1234", "Emilio",100);
+        } catch (CuentaException e1) {
+            System.out.println(e1.getMessage());
+            return;
+        }
+
+        try {
+            do {
+                System.out.println("introduce la opcion que deseas");
+                System.out.println("1) meter dinero");
+                System.out.println("2) sacar dinero");
+                System.out.println("3) consultar saldo");
+                System.out.println("4) salir");
+
+                opcion = entrada.nextInt();
+
+                if (opcion==1){
+                    System.out.println("cuento dinero quieres meter");
+                    dineroMeter = entrada.nextInt();
+                    c1.meterDinero(dineroMeter);
+                }
+
+                if (opcion==2){
+                    System.out.println("cuento dinero quieres retirar");
+                    dineroSacar = entrada.nextInt();
+                    c1.sacarDinero(dineroSacar);
+                }
+
+                if (opcion==3){
+
+                }
+            } while (opcion != 4);
+
+
+        } catch (CuentaException e2) {
+            System.out.println(e2.getMessage());
         }
     }
 
