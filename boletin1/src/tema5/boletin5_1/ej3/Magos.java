97f6544c4553896ef797f16aa934f3e2e5cf3816
pc-emarfer213 <emiliomartinf2002@gmail.com>
2025-01-17 08:52:43 +0100

    tema4

--- boletin1/src/tema5/boletin5_1/ej3/Magos.java
+++ boletin1/src/tema5/boletin5_1/ej3/Magos.java
@@ -1,5 +1,42 @@
 package tema5.boletin5_1.ej3;
 
-public class Magos {
+import tema4.personaException;
 
+public class Magos extends Personaje{
+
+    private static final int MIN_INTELIGENCIA = 17;
+    private static final int MAX_FUERZA = 15;
+    private static final int MAX_NUM_HECHIZOS = 4;
+    private static final int VIDA_POR_HECHIZOS = 10;
+
+    private String[] hechizosAprendidos;
+
+    public Magos(String nombre, TRaza raza, int fuerza, int inteligencia, int vida, int vidaMax, int vidaMax1) {
+        super(nombre, raza, fuerza, inteligencia, vida, vidaMax, vidaMax1);
+        setInteligencia(inteligencia);
+        setFuerza(fuerza);
+        hechizosAprendidos=new String[MAX_NUM_HECHIZOS];
+    }
+
+    @Override
+    public void setInteligencia(int inteligencia) throws personaException {
+        if (inteligencia<MIN_INTELIGENCIA){
+            throw new personaException("un mago no puede tener menos de el minimo");
+        }
+        super.setInteligencia(inteligencia);
+    }
+
+    public int getEspacioDisponible(String nombreHechizo) throws personaException {
+        int espacioDisponible= -1;
+        for (int i = 0; i < hechizosAprendidos.length; i++) {
+            if (nombreHechizo.equalsIgnoreCase(hechizosAprendidos[i])){
+                throw new personaException("este hechizo ya esta aprendido");
+            }
+
+            if (hechizosAprendidos[i] == null && espacioDisponible==-1){
+                espacioDisponible = i;
+            }
+        }
+        return espacioDisponible;
+    }
 }
--- boletin1/src/tema5/boletin5_1/ej3/Personaje.java
+++ boletin1/src/tema5/boletin5_1/ej3/Personaje.java
@@ -7,18 +7,22 @@ public class Personaje {
     private static final int fuerzaMinima=0;
     private static final int inteligenciaMaxima=20;
     private static final int inteligenciaMinima=0;
+    private static final int VIDA_MIN=0;
+
     private String nombre;
     private TRaza raza;
     private int fuerza;
     private int inteligencia;
     private int vida;
+    private final int vidaMax;
 
-    public Personaje(String nombre, TRaza raza, int fuerza, int inteligencia, int vida) {
+
+    public Personaje(String nombre, TRaza raza, int fuerza, int inteligencia, int vida, int vidaMax, int vidaMax1) {
         this.nombre = nombre;
         this.raza = raza;
-        this.fuerza = fuerza;
-        this.inteligencia = inteligencia;
-        this.vida = vida;
+        this.vidaMax = vidaMax1;
+        comprobarVidaMax(vidaMax);
+
     }
 
     public void setFuerza(int fuerza) throws personaException{
@@ -37,6 +41,18 @@ public class Personaje {
     }
 
     public void setVida(int vida) throws personaException {
+        if (vida<VIDA_MIN || vida>vidaMax){
+            throw new personaException("valores de vida no validos");
+        }
+
         this.vida = vida;
     }
+
+    private void comprobarVidaMax(int vidaMax) throws personaException{
+        if (vidaMax <= 0 || vidaMax >= 100) {
+            throw new personaException("valor de vida np aceptado");
+        }
+    }
+
+
 }
