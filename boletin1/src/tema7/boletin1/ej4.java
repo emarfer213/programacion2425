8849c220374c5b89b03ed997599c7e3f64232377
pc-emarfer213 <emiliomartinf2002@gmail.com>
2025-04-01 13:24:04 +0200

    tema6

--- boletin1/src/tema7/boletin1/ej4.java
+++ boletin1/src/tema7/boletin1/ej4.java
@@ -1,23 +1,22 @@
 package tema7.boletin1;
 
 import java.io.*;
+import java.nio.file.*;
 
-public class ej3 {
-
+public class ej4 {
     public static void main(String[] args) {
+
+        Path emptyfile = Path.of("boletin1/salida.txt");
         try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-             PrintWriter os = new PrintWriter(new FileWriter("salida.txt"))) {
+        ) {
 
             String l;
             while ((l = br.readLine()) != null && !l.equalsIgnoreCase("fin")) {
-                os.println(l);
-                os.flush();
+                Files.writeString(emptyfile, l + "\n", StandardOpenOption.CREATE, StandardOpenOption.APPEND);
             }
 
         } catch (IOException e) {
             System.out.println(e.getMessage());
         }
     }
-
 }
-
