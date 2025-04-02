865c85049e68b8c872bbc80bbb635458b078e6a7
pc-emarfer213 <emiliomartinf2002@gmail.com>
2025-04-02 09:25:40 +0200

    tema6

---
+++ boletin1/src/tema7/pruebas/prueba1.java
@@ -0,0 +1,35 @@
+package tema7.pruebas;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.file.Path;
+
+public class prueba1 {
+    public static void main(String[] args) throws FileNotFoundException {
+        new prueba1();
+    }
+
+    public prueba1() throws FileNotFoundException {
+        try (RandomAccessFile data = new RandomAccessFile("..\\boletin1\\src\\tema7\\pruebas\\pradera.bmp", "r");
+             FileOutputStream out = new FileOutputStream("..\\boletin1\\src\\tema7\\pruebas\\praderaInversa.bmp")){
+
+            byte[] cabecera = new byte[54];
+            data.read(cabecera);
+            out.write(cabecera);
+
+            byte[] pixel = new byte[3];
+
+            while (data.read(pixel) != -1){
+                for (int i = 0; i < pixel.length; i++) {
+                    pixel[i] = (byte) ~pixel[i];
+                }
+                out.write(pixel);
+            }
+
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
