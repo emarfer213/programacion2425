a3f471bbf5b6f2e6a027678273555964c0332126
pc-emarfer213 <emiliomartinf2002@gmail.com>
2025-05-07 10:16:43 +0200

    tema7

--- boletin1/src/tema7/boletin4/ej6.java
+++ boletin1/src/tema7/boletin4/ej6.java
@@ -9,26 +9,19 @@ import java.util.List;
 public class ej6 {
     public static void main(String[] args) {
         //File file = new File("src/tema7/boletin4/bitacoras.txt");
-
-        try {
-
-            rotarContenido();
-
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
+        rotarContenido();
     }
 
-    public static void rotarContenido(){
+    public static void rotarContenido() {
         Path rutaDestino = Path.of("src/tema7/boletin4/bitacoras.txt");
         try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
             String l;
-            while (!(l= br.readLine()).equals("fin")){
+            while (!(l = br.readLine()).equals("fin")) {
                 Files.writeString(rutaDestino, l + "\n", StandardOpenOption.APPEND, StandardOpenOption.CREATE);
-                if (Files.size(rutaDestino) > 20){
+                if (Files.size(rutaDestino) > 20) {
                     List<String> listaDeLineas = Files.readAllLines(rutaDestino);
                     listaDeLineas.removeFirst();
-                    Files.write(rutaDestino,listaDeLineas, StandardOpenOption.TRUNCATE_EXISTING);
+                    Files.write(rutaDestino, listaDeLineas, StandardOpenOption.TRUNCATE_EXISTING);
                 }
             }
         } catch (IOException e) {
