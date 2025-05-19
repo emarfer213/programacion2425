90cc1cf4a1b5a2847db03ab55beee0ef662298f4
emilio <emarfer213@g.educaand.es>
2025-05-19 14:43:09 +0200

    tema8

--- Tema8/src/main/java/boletin1/ej9.java
+++ Tema8/src/main/java/boletin1/ej9.java
@@ -1,4 +1,58 @@
 package boletin1;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.sql.*;
+
 public class ej9 {
+    public static void main(String[] args) {
+        String cadenaConexion = "jdbc:mysql://127.0.0.1:3306/classicmodels?serverTimezone=UTC";
+        try (Connection c = DriverManager.getConnection(cadenaConexion, "root", "root");
+                BufferedReader br = new BufferedReader(new InputStreamReader(System.in))){
+
+            System.out.println("introduce el nombre");
+            String nombre = br.readLine();
+
+            PreparedStatement consulta1 = c.prepareStatement("delete from customers " +
+                    "where customerName like ?; ");
+
+            consulta1.setString(1, nombre + "%");
+
+            /*while (resultado1.next()){
+                System.out.println(resultado1.getString("customerName"));
+            }*/
+
+            PreparedStatement consulta2 = c.prepareStatement("delete from payments join customers on payments.customerNumber = customers.customerNumber where customers.customerName like ? ;");
+            consulta2.setString(1, nombre + "%");
+
+            /*while (resultado2.next()){
+                System.out.println(resultado2.getDate("paymentDate"));
+            }*/
+
+            PreparedStatement consulta3 = c.prepareStatement("delete from orders join customers on orders.customerNumber = customers.customerNumber where customers.customerName like ? ;");
+            consulta3.setString(1, nombre + "%");
+
+            /*while (resultado3.next()){
+                System.out.println(resultado3.getString("comments"));
+            }*/
+
+            PreparedStatement consulta4 = c.prepareStatement("delete from orderdetails join orders on orderdetails.orderNumber = orders.orderNumber " +
+                    "join customers on orders.customerNumber = customers.customerNumber where customers.customerName like ? ;");
+            consulta4.setString(1, nombre + "%");
+
+            /*while (resultado4.next()){
+                System.out.println(resultado4.getDouble("priceEach"));
+            }*/
+
+            consulta4.executeUpdate();
+            consulta3.executeUpdate();
+            consulta2.executeUpdate();
+            consulta1.executeUpdate();
+
+        } catch (IOException | SQLException e){
+            System.out.println(e.getMessage());
+        }
+    }
+
 }
